70d87fec6081abab6b13b08892e5bfac5bb7dbc8
Pedro Oliveira <bernardespedro790@gmail.com>
2026-05-12 13:21:56 -0300

    Corrigido conflito

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,12 +2,6 @@ import { useState } from "react";
 
 function Formulario({ adicionarTarefa }) {
   const [titulo, setTitulo] = useState("");
-<<<<<<< HEAD
-  const [descricao, setDescricao] = useState("");
-  const [prioridade, setPrioridade] = useState("");
-  const [dia, setDia] = useState("");
-  const [horario, setHorario] = useState("");
-=======
   const [descricao, setDescricao] =
     useState("");
   const [prioridade, setPrioridade] =
@@ -15,7 +9,6 @@ function Formulario({ adicionarTarefa }) {
   const [dia, setDia] = useState("");
   const [horario, setHorario] =
     useState("");
->>>>>>> 3b05666409d58bb918123325c3f6ab576a6b8d96
 
   function enviarFormulario(e) {
     e.preventDefault();
@@ -96,46 +89,11 @@ function Formulario({ adicionarTarefa }) {
         }
       />
 
-      <select
-        className="form-control mb-3"
-        value={dia}
-        onChange={(e) => setDia(e.target.value)}
-      >
-        <option value="">Selecione o dia</option>
-        <option>Segunda</option>
-        <option>Terça</option>
-        <option>Quarta</option>
-        <option>Quinta</option>
-        <option>Sexta</option>
-      </select>
-
-      <input
-        className="form-control mb-3"
-        type="time"
-        value={horario}
-        onChange={(e) => setHorario(e.target.value)}
-      />
-
       <button className="btn btn-primary">
         Adicionar
       </button>
-
-<<<<<<< HEAD
-<input
-  className="form-control mb-3"
-  type="time"
-  value={horario}
-  onChange={(e) => setHorario(e.target.value)}
-/>
-
-<button className="btn btn-primary">
-  Adicionar
-</button>
-
-</form>
-);
+    </form>
+  );
 }
+
 export default Formulario;
-=======
-export default Formulario;
->>>>>>> 3b05666409d58bb918123325c3f6ab576a6b8d96
