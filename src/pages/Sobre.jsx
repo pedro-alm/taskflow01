8ebcf63bf192d03ccbfb4e96c4a3793f09629e7d
iagosouzadev <iagoalexandrerosa@gmail.com>
2026-06-09 16:11:12 -0300

    Atualiza pagina Sobre
    # Conflicts:
    #	src/pages/Sobre.jsx

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -6,8 +6,13 @@ function Sobre() {
       <p>
 
          Projeto desenvolvido que tem como objetvo organizar suas arefas diárias no seu dia a dia descrevndo qual é a sua taréfa, sua prioridade e também o dia e o horário. Feita com React, Vite, Bootstrap e localStorage.
+
          Este é um projeto de cadastro de tarefas desenvolvido com React, Vite e Bootstrap. A aplicação utiliza localStorage para armazenar as tarefas no navegador, permitindo que os dados sejam mantidos mesmo após recarregar a página. O objetivo é praticar conceitos de desenvolvimento frontend moderno, como componentização, gerenciamento de estado e manipulação de dados locais.
         (aprimorei a interface da aplicação e criei o conteúdo da página Sobre, implementei melhorias visuais na interface geral da aplicação com CSS)
+
+
+        Este é um projeto de cadastro de tarefas desenvolvido com React, Vite e Bootstrap. A aplicação utiliza localStorage para armazenar as tarefas no navegador, permitindo que os dados sejam mantidos mesmo após recarregar a página. O objetivo é praticar conceitos de desenvolvimento frontend moderno, como componentização, gerenciamento de estado e manipulação de dados locais.
+
       </p>
     </div>
   );
